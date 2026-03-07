1537ddb7570187a2f3a9bd939347634a7501a158
DENNIS MUUO <dennismuuo.dev@gmail.com>
2026-03-07 22:57:27 +0300

    feature: added widget_service.dart

---
+++ lib/services/widget_service.dart
@@ -0,0 +1,43 @@
+import 'package:home_widget/home_widget.dart';
+import 'package:intl/intl.dart';
+
+class WidgetService {
+  static const String _groupId = 'group.com.pacewisp.pacewisp'; // For iOS if needed later
+  static const String _androidWidgetName = 'AppWidgetProvider';
+
+  static Future<void> updateWidgetData({
+    required String accountName,
+    required dynamic income,
+    required dynamic entries,
+    bool isBlurred = true,
+  }) async {
+    final currencyFormat = NumberFormat("#,###", "en_US");
+    
+    String formattedIncome = "0";
+    try {
+      final double n = double.parse(income.toString());
+      formattedIncome = "KSH ${currencyFormat.format(n.toInt())}";
+    } catch (e) {
+      formattedIncome = income.toString();
+    }
+
+    await HomeWidget.saveWidgetData<String>('account_name', accountName);
+    await HomeWidget.saveWidgetData<String>('income', formattedIncome);
+    await HomeWidget.saveWidgetData<String>('entries', entries.toString());
+    await HomeWidget.saveWidgetData<bool>('is_blurred', isBlurred);
+    
+    await HomeWidget.updateWidget(
+      name: _androidWidgetName,
+      androidName: _androidWidgetName,
+    );
+  }
+
+  static Future<void> toggleBlur() async {
+    final bool? currentBlur = await HomeWidget.getWidgetData<bool>('is_blurred', defaultValue: true);
+    await HomeWidget.saveWidgetData<bool>('is_blurred', !(currentBlur ?? true));
+    await HomeWidget.updateWidget(
+      name: _androidWidgetName,
+      androidName: _androidWidgetName,
+    );
+  }
+}
