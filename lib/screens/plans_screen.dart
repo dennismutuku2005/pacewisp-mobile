081e56ede25753e444980767fc16265084107f25
DENNIS MUUO <dennismuuo.dev@gmail.com>
2026-03-07 20:13:32 +0300

    update: refined plans_screen.dart functionality

--- lib/screens/plans_screen.dart
+++ lib/screens/plans_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
 import '../providers/settings_provider.dart';
 import '../services/api_service.dart';
