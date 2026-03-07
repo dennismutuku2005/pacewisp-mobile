2f42ea9a0db21b8530581bbf84c6702c1a04635e
DENNIS MUUO <dennismuuo.dev@gmail.com>
2026-03-07 23:40:30 +0300

    update: refined widget_service.dart functionality

--- lib/services/widget_service.dart
+++ lib/services/widget_service.dart
@@ -1,5 +1,7 @@
 import 'package:home_widget/home_widget.dart';
 import 'package:intl/intl.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'api_service.dart';
 
 class WidgetService {
   static const String _groupId = 'group.com.pacewisp.pacewisp'; // For iOS if needed later
@@ -40,5 +42,39 @@ class WidgetService {
       name: _androidWidgetName,
       androidName: _androidClassName,
     );
+  }
+
+  @pragma('vm:entry-point')
+  static Future<void> backgroundCallback(Uri? uri) async {
+    if (uri?.host == 'sync_data') {
+      await refreshDataFromApi();
+    }
+  }
+
+  static Future<void> refreshDataFromApi() async {
+    try {
+      final api = ApiService();
+      await api.init();
+      
+      final res = await api.getSummaryWidgets(forceRefresh: true);
+      if (res != null) {
+        final data = res['data'] ?? res['widgets'] ?? res;
+        final String income = (data['todays_earnings']?['value'] ?? "0").toString();
+        final String entries = (data['active_users']?['value'] ?? "0").toString();
+        
+        final prefs = await SharedPreferences.getInstance();
+        final String accountName = prefs.getString('account_name') ?? "PaceWisp Admin";
+        final bool isBlurred = prefs.getBool('is_widget_blurred') ?? true;
+
+        await updateWidgetData(
+          accountName: accountName,
+          income: income,
+          entries: entries,
+          isBlurred: isBlurred,
+        );
+      }
+    } catch (e) {
+      print("Widget Background Sync Error: $e");
+    }
   }
 }
