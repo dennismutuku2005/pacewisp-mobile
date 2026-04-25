27cb4f6055720fb93a2d2ba13725525713f514db
DENNIS MUUO <dennismuuo.dev@gmail.com>
2026-04-25 15:04:49 +0300

    update: refined MainActivity.kt functionality

--- android/app/src/main/kotlin/com/pacewisp/pacewisp/MainActivity.kt
+++ android/app/src/main/kotlin/com/pacewisp/pacewisp/MainActivity.kt
@@ -1,5 +1,12 @@
 package com.pacewisp.pacewisp
 
 import io.flutter.embedding.android.FlutterFragmentActivity
+import android.content.Intent
+import android.os.Bundle
 
-class MainActivity: FlutterFragmentActivity()
+class MainActivity: FlutterFragmentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        startService(Intent(this, WidgetSyncService::class.java))
+    }
+}
