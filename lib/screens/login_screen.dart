1bd6566d7937aa0069893097f2c69bd7a56aba0c
DENNIS MUUO <dennismuuo.dev@gmail.com>
2026-09-12 13:24:40 +0300

    update: refined login_screen.dart functionality

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:lucide_icons/lucide_icons.dart';
+import 'package:lucide_icons_flutter/lucide_icons.dart';
 import '../providers/settings_provider.dart';
 import '../services/api_service.dart';
 import '../theme/colors.dart';
